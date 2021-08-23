8683c514cf405dd50817460d4d7091fc6647ad73
wassim <b.wassim1993@hotmail.fr>
2021-08-23 02:17:46 +0100

    project setup

---
+++ InfiniteScrollExample/InfiniteScrollExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfiniteScrollExample
+//
+//  Created by wassim on 23/8/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InfiniteScrollExample/InfiniteScrollExample/InfiniteScrollExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollExampleApp.swift
+//  InfiniteScrollExample
+//
+//  Created by wassim on 23/8/2021.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
